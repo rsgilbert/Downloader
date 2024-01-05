b4caeb1fb16e6fdb7e7e08f58382256b82009337
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-01-05 10:24:27 +0300

    Setup a simpler scheduler that just waits for 10s

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Specialized;
+using Quartz;
+using Quartz.Impl;
+
 Console.WriteLine("Hello, World!");
+
+StdSchedulerFactory factory = new StdSchedulerFactory();
+IScheduler scheduler = await factory.GetScheduler();
+
+await scheduler.Start();
+Console.WriteLine("Starting scheduler");
+
+
+// some sleep to show whats happening
+await Task.Delay(TimeSpan.FromSeconds(10));
+
+Console.WriteLine("Shutting down scheduler");
+await scheduler.Shutdown();
